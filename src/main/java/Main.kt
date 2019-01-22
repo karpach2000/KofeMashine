4054d920509c7a6f8f8e84ddc61d18179429b4ef
Павел Чурляев <churljaev.p@lokasoft.ru>
2019-01-22 21:53:10 +0300

    Проверка лицензии

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,8 +1,19 @@
-import  com.parcel.Board
+import com.parcel.Board
+import com.parcel.license.LicenseChecker
+
 fun main(){
-    val board = Board()
-    board.update()
-    board.generate()
-    while(true)
-        Thread.sleep(100)
+
+    if(LicenseChecker.checkLicense()) {
+        println("Лицензия принята. Запуск программы")
+
+        val board = Board()
+        board.update()
+        board.generate()
+        while(true)
+            Thread.sleep(100)
+    } else {
+        println("Лицензия не принята. Заплатите разработчикам программы, чтобы получить лицензию")
+    }
+
+
 }
