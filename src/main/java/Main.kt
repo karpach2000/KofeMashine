0a5465e999f1fe2cace83a9eb94265eb7a343a70
Павел Чурляев <churljaev.p@lokasoft.ru>
2019-01-22 21:59:47 +0300

    Revert "Проверка лицензии"
    This reverts commit 4054d920509c7a6f8f8e84ddc61d18179429b4ef.

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,19 +1,8 @@
-import com.parcel.Board
-import com.parcel.license.LicenseChecker
-
+import  com.parcel.Board
 fun main(){
-
-    if(LicenseChecker.checkLicense()) {
-        println("Лицензия принята. Запуск программы")
-
-        val board = Board()
-        board.update()
-        board.generate()
-        while(true)
-            Thread.sleep(100)
-    } else {
-        println("Лицензия не принята. Заплатите разработчикам программы, чтобы получить лицензию")
-    }
-
-
+    val board = Board()
+    board.update()
+    board.generate()
+    while(true)
+        Thread.sleep(100)
 }
